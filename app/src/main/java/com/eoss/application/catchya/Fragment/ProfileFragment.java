3bd009aabe172249b12bcb35263c8dd59dce7180
fourfore <lulllaby.fore@gmail.com>
2016-09-20 15:35:05 +0700

    profile fragment

--- app/src/main/java/com/eoss/application/catchya/Fragment/ProfileFragment.java
+++ app/src/main/java/com/eoss/application/catchya/Fragment/ProfileFragment.java
@@ -3,13 +3,23 @@ package com.eoss.application.catchya.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.eoss.application.catchya.R;
+import com.google.android.gms.vision.text.Text;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,16 +38,34 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
     @Override
     public void onStart()
     {
+        final ImageView profilePic = (ImageView)getView().findViewById(R.id.person_photo);
+        final TextView name = (TextView)getView().findViewById(R.id.person_name);
+        final TextView email = (TextView)getView().findViewById(R.id.person_email);
         super.onStart();
+        DatabaseReference mCurrentUser = mDatabase.child("Users").child(mAuth.getCurrentUser().getUid());
+        mCurrentUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("Foremost onDataChange ",dataSnapshot.toString());
+                name.setText(dataSnapshot.child("Name").getValue().toString());
+                email.setText(dataSnapshot.child("Email").getValue().toString());
+                Picasso.with(getContext()).load(dataSnapshot.child("Pic").getValue().toString()).into(profilePic);
 
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
     }
 
 
