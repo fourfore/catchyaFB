1c8243444e2f51aa21e857c86f6fe785ef16558b
eoss-th <contact@eoss-th.com>
2016-09-26 13:46:56 +0700

    ViewSwatcher

--- app/src/main/java/com/eoss/application/catchya/Fragment/ProfileFragment.java
+++ app/src/main/java/com/eoss/application/catchya/Fragment/ProfileFragment.java
@@ -7,8 +7,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import com.eoss.application.catchya.R;
 import com.google.android.gms.vision.text.Text;
@@ -21,6 +26,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import static android.R.id.button1;
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -28,7 +35,10 @@ public class ProfileFragment extends Fragment {
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
-
+    private ViewSwitcher viewSwitcher;
+    private RelativeLayout myFirstView;
+    private RelativeLayout mySecondView;
+    private LinearLayout wrapper;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -49,6 +59,9 @@ public class ProfileFragment extends Fragment {
         final ImageView profilePic = (ImageView)getView().findViewById(R.id.person_photo);
         final TextView name = (TextView)getView().findViewById(R.id.person_name);
         final TextView email = (TextView)getView().findViewById(R.id.person_email);
+        final EditText editName = (EditText) getView().findViewById(R.id.person_name_edit);
+        final EditText editEmail = (EditText) getView().findViewById(R.id.person_email_edit);
+
         super.onStart();
         DatabaseReference mCurrentUser = mDatabase.child("Users").child(mAuth.getCurrentUser().getUid());
         mCurrentUser.addValueEventListener(new ValueEventListener() {
@@ -57,8 +70,9 @@ public class ProfileFragment extends Fragment {
                 Log.d("Foremost onDataChange ",dataSnapshot.toString());
                 name.setText(dataSnapshot.child("Name").getValue().toString());
                 email.setText(dataSnapshot.child("Email").getValue().toString());
+                editName.setText(dataSnapshot.child("Name").getValue().toString());
+                editEmail.setText(dataSnapshot.child("Email").getValue().toString());
                 Picasso.with(getContext()).load(dataSnapshot.child("Pic").getValue().toString()).into(profilePic);
-
             }
 
             @Override
@@ -66,7 +80,29 @@ public class ProfileFragment extends Fragment {
 
             }
         });
+
+
+
+        viewSwitcher =   (ViewSwitcher) getView().findViewById(R.id.mSwitchProfileEdit);
+        myFirstView= (RelativeLayout ) getView().findViewById(R.id.profileTextView);
+        mySecondView = (RelativeLayout) getView().findViewById(R.id.profileTextEdit);
+        wrapper = (LinearLayout) getView().findViewById(R.id.wrapper);
+        wrapper.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                if (viewSwitcher.getCurrentView() != myFirstView){
+
+                    viewSwitcher.showPrevious();
+                } else if (viewSwitcher.getCurrentView() != mySecondView){
+
+                    viewSwitcher.showNext();
+                }
+            }
+        });
     }
+
 
 
 
